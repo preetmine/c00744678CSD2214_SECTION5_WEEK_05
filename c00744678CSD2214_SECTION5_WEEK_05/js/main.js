294b34dcd9db1b00279cb99a39815ddeb16ff8f1
visualstudio <visualstudio@230-PC35>
2019-02-07 12:02:07 -0500

    changes

--- c00744678CSD2214_SECTION5_WEEK_05/js/main.js
+++ c00744678CSD2214_SECTION5_WEEK_05/js/main.js
@@ -1,1 +1,17 @@
 ﻿// Your code here!
+function myfunction(a, b) {
+   return a+b;   // the function return the sum
+}
+function arrrays() {
+    var pens;
+    pens = ["red", "blue", "orange", "green", "black"];
+    console.log(pens);
+   
+    for (x = 0; x <= pens.length; x++) {
+        alert(pens[x]);
+       
+    }
+    alert("the length of array is " + pens.length);
+
+   
+}
